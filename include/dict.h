18b6030a9f317e531a94e39c02bfaa4098fe2861
ghosind <ghosind@gmail.com>
2019-01-20 17:15:31 +0800

    add dict

---
+++ include/dict.h
@@ -0,0 +1,28 @@
+/*
+ * dict.h - Corgi DB
+ * 
+ * Copyright (C) 2019, Chen Su <ghosind@gmail.com>. All right reserved.
+ */
+
+typedef struct {
+  char *key;
+  char *value;
+  DictNode *next;
+} DictNode;
+
+typedef struct {
+  int size;
+  int mask;
+  int used;
+  DictNode **table;
+  int (*hash_function)(const char *key);
+} Dict;
+
+Dict *dict_init();
+void dict_reset(Dict *dict);
+
+void dict_expand(Dict *dict, const unsigned int size);
+DictNode *dict_find(Dict *dict, const char *key);
+
+char *dict_get(Dict *dict, const char *key);
+int dict_set(Dict *dict, const char *key, const char *value);
---
+++ src/dict.c
@@ -0,0 +1,99 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <dict.h>
+#include <error.h>
+
+void dict_reset(Dict* dict) {
+  dict->size = 0;
+  dict->mask = 0;
+  dict->used = 0;
+  dict->table = NULL;
+}
+
+Dict *dict_init() {
+  Dict* dict;
+
+  dict = (Dict *) malloc(sizeof(Dict));
+  if (!dict) {
+    db_error(1, "Failed to allocate memory for dictionary.");
+  }
+
+  dict_reset(dict);
+
+  return dict;
+}
+
+void dict_expand(Dict *dict, const unsigned int size) {
+  DictNode **new_table;
+  int used = dict->used;
+
+  new_table = (DictNode **) malloc(sizeof(DictNode) * size);
+  if (!new_table) {
+    db_error(1, "");
+  }
+
+  for (int i = 0; i < dict->size && used > 0; i++) {
+    DictNode *node = dict->table[i];
+
+    while (node) {
+      int hash_key = dict->hash_function(node->key) % dict->mask;
+      DictNode *new = node;
+      node = node->next;
+
+      new->next = new_table[hash_key];
+      new_table[hash_key] = new;
+      used--;
+    }
+  }
+
+  free(dict->table);
+
+  dict->size = size;
+  dict->mask = size;
+  dict->table = new_table;
+}
+
+DictNode *dict_find(Dict *dict, const char *key) {
+  int hash_key = dict->hash_function(key) % dict->mask;
+  DictNode *node = dict->table[hash_key];
+
+  while (node) {
+    if (strcmp(node->key, key)) {
+      break;
+    }
+
+    node = node->next;
+  }
+
+  return node;
+}
+
+char *dict_get(Dict *dict, const char *key) {
+  DictNode *node = dict_find(dict, key);
+
+  return node ? node->key : NULL;
+}
+
+int dict_set(Dict *dict, const char *key, const char *value) {
+  DictNode *node = dict_find(dict, key);
+
+  if (node) {
+    node->value = value;
+    return 0;
+  }
+
+  if (dict->used == dict->size) {
+    dict_expand(dict, dict->size * 2);
+  }
+
+  int hash_key = dict->hash_function(key);
+  DictNode *new_node = (DictNode *) malloc(sizeof(DictNode));
+
+  new_node->key = key;
+  new_node->value = value;
+  new_node->next = dict->table[hash_key];
+  dict->table[hash_key] = new_node;
+
+  return 0;
+}
